883b83825acdd51e8ad74d35a355977749c6f78f
mubir <info.mubir@gmail.com>
2024-03-24 15:03:53 +0900

    #

---
+++ src/main/java/LC/WordLadder127.java
@@ -0,0 +1,70 @@
+package LC;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class WordLadder127 {
+
+    public int ladderLength(String start ,String end,String[] dic) {
+        int result = 0;
+        HashSet<String> holder = new HashSet<>();
+        for (String x : dic) {
+            holder.add(x);
+        }
+        result = BFS(start,end,holder,result);
+        return result;
+    }
+
+    public static int BFS(String start, String end, HashSet<String> holder, int result) {
+
+        Queue<String> q = new LinkedList();
+        q.add(start);
+
+        while (q.isEmpty() == false) {
+            for (int k = 0; k < q.size(); k++) {
+                String word = q.poll();
+                char[] current = word.toCharArray();
+                for (int i = 0; i < word.length(); i++) {
+                    char restore = current[i];
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        if(ch == restore)
+//                        if(ch == current[i])
+                            continue;
+                        current[i] = ch;
+                        String cand = new String(current);
+                        if (holder.contains(cand)) {
+                           q.add(cand);
+                           holder.remove(cand);
+                        }
+                        if (cand.equals(end)) {
+                            return result+1;
+                        }
+                    }
+                    current[i] = restore;
+                }
+
+            }
+            result++;
+
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String beginWord = "hit";
+        String endWord = "cog";
+        String[] wordList = {"hot", "dot", "dog", "lot", "log", "cog"};
+
+        WordLadder127 obj = new WordLadder127();
+        System.out.println(obj.ladderLength(beginWord,endWord,wordList)); // out : 5
+
+        String beginWord2 = "hit";
+
+        String endWord2 = "cog";
+        String[] wordList2 = {"hot","dot","dog","lot","log"};
+
+
+        System.out.println(obj.ladderLength(beginWord2,endWord2,wordList2)); // output:
+    }
+}
